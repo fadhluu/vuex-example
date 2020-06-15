07929e5b95cbea8321a09b90b16e953192b97d9e
fadhlu <fadhlurahman.i@gmail.com>
2020-06-15 13:57:32 +0700

    better folder structures

--- src/store/store.js
+++ src/store/store.js
@@ -2,49 +2,14 @@
 /* eslint-disable import/prefer-default-export */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter';
+import value from './modules/value';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  state: {
-    counter: 0,
-    value: 0,
-  },
-  getters: {
-    doubleCounter: state => state.counter * 2,
-    clickCounter: state => `${state.counter} clicks`,
-    value: state => state.value,
-  },
-  mutations: {
-    increment: (state, payload) => {
-      state.counter += payload;
-    },
-    decrement: (state, payload) => {
-      state.counter -= payload;
-    },
-    setValue: (state, payload) => {
-      state.value = payload;
-    },
-  },
-  actions: {
-    increment: ({ commit }, payload) => {
-      commit('increment', payload);
-    },
-    decrement: ({ commit }, payload) => {
-      commit('decrement', payload);
-    },
-    asyncIncrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('increment', payload.by);
-      }, payload.duration);
-    },
-    asyncDecrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('decrement', payload.by);
-      }, payload.duration);
-    },
-    setValue: ({ commit }, payload) => {
-      commit('setValue', payload);
-    },
+  modules: {
+    counter,
+    value,
   },
 });
